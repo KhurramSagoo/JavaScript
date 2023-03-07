79c8d1af0f1d2d758187a748a9a3305fae3e03f2
khurramsagoo <uaf.khurram@gmail.com>
2023-03-07 08:52:13 -0800

    filter complete

--- FilterMethod.js
+++ FilterMethod.js
@@ -1,3 +1,24 @@
 // filter is used to filter arrray 
 // in simple words, filter is used to remove items from existing array
 // it always return a new array
+
+// const ages= [5,23,14,30,22];
+// console.log(ages);
+
+// const adults= ages.filter((age)=>{
+// return age>18;
+// })
+// console.log(adults)
+
+// to simplify
+const ages= [5,23,14,30,22];
+console.log(ages);
+
+const adults= ages.filter((age)=>
+ age!==30)
+console.log(adults)
+
+// note which element pass this condition , that will add in new array
+const nadults= ages.filter((age)=>
+ age>18)
+console.log(nadults)
